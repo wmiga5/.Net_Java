a2102748fd2537560eca27082e1247af7a55ef3c
wmiga5 <wojtek.miga@gmail.com>
2024-02-29 16:28:54 +0100

    Simple Fizz_Buzz_app

---
+++ ConsoleApp1/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class FizzBuzz
+    {
+        int upper_limit;
+        public FizzBuzz(int x) { upper_limit = x; }
+        public void DisplayAttribute()
+        {
+            for (int i = 1; i <= upper_limit; i++)
+            {
+                if (i%3 == 0 && i%5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           FizzBuzz fz=new FizzBuzz(20);
+            fz.DisplayAttribute();
+
+        }
+    }
+}
